0bc1f95838a3ef4f977f4ea5f44be476207a5ab2
Ramiz Raja <ramizraja97@gmail.com>
2014-04-14 11:57:10 +0500

    Issue #3, title error fix.

--- lib/group_owners_patches.rb
+++ lib/group_owners_patches.rb
@@ -1,7 +1,5 @@
 #patches
-Rails.configuration.to_prepare do
-  require 'group_owners/patches/group_patch'
-  require 'group_owners/patches/user_patch'
-  require 'group_owners/patches/groups_helper_patch'
-  require 'group_owners/patches/groups_controller_patch'
-end
+require 'group_owners/patches/group_patch'
+require 'group_owners/patches/user_patch'
+require 'group_owners/patches/groups_helper_patch'
+require 'group_owners/patches/groups_controller_patch'
